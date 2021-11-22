e3dee367984004cad8b606c965133f1f798f635f
priyo000 <priyonewvky@gmail.com>
2021-11-22 22:58:38 +0700

    1

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Status;
+
+class StatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    //mendapatkan id status yg diinginkan
+    public function getIdStatus($idstatus)
+    {
+        $data = Status::find($idstatus);
+        return response()->json($data, 200);
+    }
+
+
+    //menampilkan data
+    public function index()
+    {
+        //mendapatkan semua data dari tabel statuses
+        $data = Status::all();
+        return response()->json($data, 200);
+    }
+
+
+}
